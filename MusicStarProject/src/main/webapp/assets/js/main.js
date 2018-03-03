28c13ec8bcca588f8f4e41a2e58d0be9240cd7bc
陳聖豐 <eeit9908@gmail.com>
2018-03-03 21:12:33 +0800

    修正BUG
    index.jsp: 移除多餘的標籤 <div id="main_container"></div>
    slideshow.js, main.js: 調整 Disable animations/transitions的執行順序

--- MusicStarProject/src/main/webapp/assets/js/main.js
+++ MusicStarProject/src/main/webapp/assets/js/main.js
@@ -1,4 +1,14 @@
 $(function() { 
+	
+    // Disable animations/transitions until the page has loaded.
+    $body.addClass('is-loading');
+
+    $window.on('load', function () {
+        window.setTimeout(function () {
+            $body.removeClass('is-loading');
+        }, 100);
+    });
+	
 	// Scrolly.
 	if ($(".scrolly").length) {
 
--- MusicStarProject/src/main/webapp/assets/js/slideshow.js
+++ MusicStarProject/src/main/webapp/assets/js/slideshow.js
@@ -258,14 +258,7 @@ var settings = {
 			$body = $('body'),
 			$slideshow = $('.slideshow');
 
-        // Disable animations/transitions until the page has loaded.
-        $body.addClass('is-loading');
 
-        $window.on('load', function () {
-            window.setTimeout(function () {
-                $body.removeClass('is-loading');
-            }, 100);
-        });
 
         // Prioritize "important" elements on medium.
         skel.on('+medium -medium', function () {
