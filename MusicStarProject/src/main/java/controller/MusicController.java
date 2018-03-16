bbddce16f0f639f86e05692e7873f584d2bab4c1
蒲怡仁 <james.pu@outlook.com>
2018-03-16 10:24:51 +0800

    修正 加入@Autowired MusicStyleService

--- MusicStarProject/src/main/java/controller/MusicController.java
+++ MusicStarProject/src/main/java/controller/MusicController.java
@@ -26,13 +26,15 @@ import _global.utils.Parser;
 import model.bean.MemberBean;
 import model.bean.MusicBean;
 import model.service.MusicService;
+import model.service.MusicStyleService;
 
 @Controller
 public class MusicController
 {
 	@Autowired
 	MusicService musicservice;
-
+	@Autowired
+	MusicStyleService musicStyleService;
 	@Autowired
 	String profilesDirectoryPath;
 	@Autowired
@@ -171,7 +173,7 @@ public class MusicController
 	   } 
      	return null;
 	}
-    }
+    
 	
 	/**
 	 * 取得音樂型態的Map<styleId,styleName>
